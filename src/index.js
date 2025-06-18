46772ee2055e35e02230814140e4ccfa94bcf9d8
santosh312-dev <santosh.sharma.dev312@gmail.com>
2025-06-18 16:52:59 +0545

    utils

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+.then(()=>{
+  app.on("error",(error)=>{
+    console.log("ERROR: ",error);
+    throw error;
+  })
+  app.listen(process.env.PORT || 8000,()=>{
+    console.log(`Server is running on : localhost: ${process.env.PORT}`);
+    
+  })
+})
+.catch((error)=>{
+  console.log("MongoDB connection Fail !!!",error);
+  
+})
 
 /*
 import { DB_NAME } from "./constants";
